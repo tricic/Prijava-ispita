112f9d5730d240fd0e55915476910f96290c0167
tricic <ismar.tricic@gmail.com>
2020-01-15 17:04:22 +0100

    Prebacivanje skripta.js u static folder

--- partials/head.php
+++ partials/head.php
@@ -2,5 +2,5 @@
     <title>Prijava ispita</title>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="static/style.css">
-    <script src="skripta.js"></script>
+    <script src="static/skripta.js"></script>
 </head>
