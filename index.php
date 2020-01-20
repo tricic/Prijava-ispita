e83df1a83ead8817d5b7f02539090108113f1dde
tricic <ismar.tricic@gmail.com>
2020-01-20 21:19:39 +0100

    Prebacivanje HTML-a u PHP kod

--- index.php
+++ index.php
@@ -1,47 +1,57 @@
 <?php
 
+// Namespace autoloading
 spl_autoload_register();
+
+// Startanje sesije
 session_start();
+
+// Postavljanje vremenske zone
 date_default_timezone_set("Europe/Sarajevo");
+
+// Učitavanje globalnih funkcija
 require("funkcije.php");
 
+// Učitavanje akcije i relativne putanje do skripte
 $akcija = !empty($_GET["akcija"]) ? $_GET["akcija"] : "pocetna";
 $fajl = "stranice/" . $akcija . ".php";
 
-// Napravi prenesene varijable
+// Kreiranje prenesenih varijabli
 foreach ($_SESSION["prenesene_varijable"] ?? [] as $key => $value)
 {
     $$key = $value;
 }
 unset($_SESSION["prenesene_varijable"]);
 
-?>
-<html>
-    <?php require("partials/head.php") ?>
-    
-    <body>
-        <?php
-            require("partials/header.php");
-            
-            if (file_exists($fajl))
-            {
-                try
-                {
-                    include($fajl);
-                }
-                catch (Exception $e)
-                {
-                    $greska = $e->getMessage();
-                    require("partials/greska.php");
-                }
-            }
-            else
-            {
-                $greska = "Stranica ne postoji!";
-                require("partials/greska.php");
-            }
+// HTML
+echo "<html>";
 
-            require("partials/footer.php");
-        ?>
-    </body>
-</html>
+require("partials/head.php");
+
+echo "<body>";
+
+
+if (file_exists($fajl))
+{
+    try
+    {
+        require("partials/header.php");
+        include($fajl);
+    }
+    catch (Exception $e)
+    {
+        $greska = $e->getMessage();
+        require("partials/greska.php");
+    }
+}
+else
+{
+    $greska = "Stranica ne postoji!";
+    require("partials/greska.php");
+}
+
+require("partials/footer.php");
+
+echo "</body>";
+
+echo "</html>";
