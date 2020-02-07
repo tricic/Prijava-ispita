f1c07db5dd8b963aba3f5d72f175cb2e377256ed
Ismar <ismar.tricic@gmail.com>
2020-02-07 10:35:58 +0100

    Link ka pocetnoj stranici izmijenjen

--- partials/header.php
+++ partials/header.php
@@ -11,7 +11,7 @@ use Helpers\Auth;
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary p-0 px-2 mb-3">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/">Početna</a>
+                <a class="nav-link" href="?akcija=pocetna">Početna</a>
             </li>
 
             <?php if (Auth::korisnikJeAdmin()) : ?>
